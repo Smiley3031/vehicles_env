f18eb646630b13ad78da4c5a5712b2e7ac5b58d2
Smiley3031 <aurelioajualip@gmail.com>
2024-01-21 18:38:25 -0600

    pagina web

---
+++ app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+fig = px.histogram(car_data, x="odometer") # crear un histograma
+fig.show() # crear gráfico de dispersión 
